62cd785a057b461e6262c1e4abbfae8e1098b904
Xadimetov Alisher Axmatboy o'g'li <akhadimetov@mbpbeelinemac2.bee.unitel.local>
2025-04-17 13:54:55 +0500

    Initial Commit

---
+++ cmd/web/handlers.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"golangs.org/snippetbox/pkg/models"
+)
+
+// Обработчик главной странице.
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		app.notFound(w)
+		return
+	}
+
+	s, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	app.render(w, r, "home.page.tmpl", &templateData{
+		Snippets: s,
+	})
+
+}
+
+// Обработчик для отображения содержимого заметки.
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+
+	s, err := app.snippets.Get(id)
+
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	app.render(w, r, "show.page.tmpl", &templateData{
+		Snippet: s,
+	})
+
+}
+
+// Обработчик для создания новой заметки.
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+	title := "Story of the Snail"
+	content := "Lorem"
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+
+}
